d69499e8a84d06f1271cd2b15adec0b56f6f85ed
tiagofrancarita <tiagofranca.rita@gmail.com>
2020-06-12 12:01:24 -0300

    Metodos Gets and Setrs da classe Contrato

--- prjCursoJava/src/br/com/franca/model/Contrato.java
+++ prjCursoJava/src/br/com/franca/model/Contrato.java
@@ -19,6 +19,107 @@ public class Contrato {
 	private int situacao;
 	private String matricula;
 	private int condicaoContrato;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Aluno getAluno() {
+		return aluno;
+	}
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+	public Turma getTurma() {
+		return turma;
+	}
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+	public double getTaxaMatricula() {
+		return taxaMatricula;
+	}
+	public void setTaxaMatricula(double taxaMatricula) {
+		this.taxaMatricula = taxaMatricula;
+	}
+	public double getValorCurso() {
+		return valorCurso;
+	}
+	public void setValorCurso(double valorCurso) {
+		this.valorCurso = valorCurso;
+	}
+	public double getDescontoCurso() {
+		return descontoCurso;
+	}
+	public void setDescontoCurso(double descontoCurso) {
+		this.descontoCurso = descontoCurso;
+	}
+	public int getQtdParcelasCurso() {
+		return qtdParcelasCurso;
+	}
+	public void setQtdParcelasCurso(int qtdParcelasCurso) {
+		this.qtdParcelasCurso = qtdParcelasCurso;
+	}
+	public double getValorMaterial() {
+		return valorMaterial;
+	}
+	public void setValorMaterial(double valorMaterial) {
+		this.valorMaterial = valorMaterial;
+	}
+	public int getQtdParcelasMaterial() {
+		return qtdParcelasMaterial;
+	}
+	public void setQtdParcelasMaterial(int qtdParcelasMaterial) {
+		this.qtdParcelasMaterial = qtdParcelasMaterial;
+	}
+	public int getDiaVencimento() {
+		return diaVencimento;
+	}
+	public void setDiaVencimento(int diaVencimento) {
+		this.diaVencimento = diaVencimento;
+	}
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+	public Date getDtMatricula() {
+		return dtMatricula;
+	}
+	public void setDtMatricula(Date dtMatricula) {
+		this.dtMatricula = dtMatricula;
+	}
+	public int getSituacao() {
+		return situacao;
+	}
+	public void setSituacao(int situacao) {
+		this.situacao = situacao;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public int getCondicaoContrato() {
+		return condicaoContrato;
+	}
+	public void setCondicaoContrato(int condicaoContrato) {
+		this.condicaoContrato = condicaoContrato;
+	}
+	@Override
+	public String toString() {
+		return "Contrato [id=" + id + ", aluno=" + aluno + ", turma=" + turma + ", taxaMatricula=" + taxaMatricula
+				+ ", valorCurso=" + valorCurso + ", descontoCurso=" + descontoCurso + ", qtdParcelasCurso="
+				+ qtdParcelasCurso + ", valorMaterial=" + valorMaterial + ", qtdParcelasMaterial=" + qtdParcelasMaterial
+				+ ", diaVencimento=" + diaVencimento + ", formaPagamento=" + formaPagamento + ", dtMatricula="
+				+ dtMatricula + ", situacao=" + situacao + ", matricula=" + matricula + ", condicaoContrato="
+				+ condicaoContrato + "]";
+	}
+	
+	
 	
 
 }
